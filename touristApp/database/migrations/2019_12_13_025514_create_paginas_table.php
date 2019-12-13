7dcc46db1d9f9e99e128c1eb278cf3a3cdea9244
camiloayza23 <camila.loayzab@gmail.com>
2019-12-12 23:53:35 -0400

    Añadiendo migraciones y BDD

--- touristApp/database/migrations/2019_12_13_025514_create_paginas_table.php
+++ touristApp/database/migrations/2019_12_13_025514_create_paginas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreatePaginasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email',30)->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('paginas', function (Blueprint $table) {
+            $table->bigIncrements('pagina_id');
+            $table->bigInteger('blog_id')->unsigned(); 
+            $table->foreign('blog_id')->references('blog_id')->on('blogs');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('paginas');
     }
 }
