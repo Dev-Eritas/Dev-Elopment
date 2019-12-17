463fa8a8481d124fe9bd0ffc716248e490b80a6c
Mariana <luzmarianacarlo1999@gmail.com>
2019-12-17 08:57:56 -0400

    adding web con todas las paginas

--- touristApp/routes/web.php
+++ touristApp/routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*-------------------------  USUARIO NORMAL  --------------------------*/
 Route::get('/', function () {
     return view('welcome');
 });
@@ -19,9 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/admin', function(){
-    return view('admin.adminHome');
-});
 
 Route::get('/sobreBolivia', function () {
     return view('sobreBolivia');
@@ -37,6 +34,18 @@ Route::get('/ciudad', function (){
 
 Route::get('/hoteles', function (){
     return view('hoteles');
+});
+
+Route::get('/hotelSalar1', function (){
+    return view('hotelSalar1');
+});
+
+Route::get('/hotelSalar2', function (){
+    return view('hotelSalar2');
+});
+
+Route::get('/hotelSalar3', function (){
+    return view('hotelSalar3');
 });
 
 Route::get('/blog', function (){
@@ -58,3 +67,41 @@ Route::get('/experiencias', function (){
 Route::get('/blog', function (){
     return view('blog');
 });
+
+Route::get('/eventosCulturales', function (){
+    return view('eventosCulturales');
+});
+
+/*-----------------------------  ADMINISTRADOR  ---------------------------------*/
+Route::get('/admin', function(){
+    return view('admin.adminHome');
+});
+
+Route::get('/perfil', function(){
+    return view('admin.perfil');
+});
+
+Route::get('/actualizaInfo', function(){
+    return view('admin.actualizaInfo');
+});
+
+Route::get('/registrarAdmin', function(){
+    return view('admin.registrarAdmin');
+});
+
+Route::get('/editor', function(){
+    return view('admin.editor');
+});
+
+Route::get('/listaPaginas', function(){
+    return view('admin.listaPaginas');
+});
+
+Route::get('/paginaOficial', function(){
+    return view('welcome');
+});
+
+Route::get('/contactosDevs', function(){
+    return view('admin.contactosDev');
+});
+
